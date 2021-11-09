6c0cc5138672243994ca039e0fc2f6f9d9db7197
Aliona990220 <aliona.havela@gmail.com>
2021-11-10 01:43:23 +0200

    first project , hello

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ package firstproject;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("My name is Aliona");
+        System.out.println("My name is Aliona.");
     }
 }
